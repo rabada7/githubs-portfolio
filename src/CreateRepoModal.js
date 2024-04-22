800e3df0877641d12c2cca0472d173274359b260
Onyebuchi Osamuta <136612873+rabada7@users.noreply.github.com>
2024-04-22 23:56:22 +0100

    Update CreateRepoModal.js

--- src/CreateRepoModal.js
+++ src/CreateRepoModal.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, ModalFooter, Button, FormControl, FormLabel, Input } from "@chakra-ui/react";
 
-const CreateRepoModal = ({ isOpen, onRequestClose }) => {
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // 
+const CreateRepoModal = ({ isOpen, onRequestClose, onCreate }) => {
+  const [repoName, setRepoName] = useState('');
+
+  const handleSubmit = () => {
+    // Validate form inputs
+    if (!repoName.trim()) {
+      alert('Please enter a repository name.');
+      return;
+    }
+
+   
+    onCreate(repoName);
+    
+    setRepoName('');
+    onRequestClose();
   };
 
   return (
@@ -14,16 +25,13 @@ const CreateRepoModal = ({ isOpen, onRequestClose }) => {
         <ModalHeader>Create New Repository</ModalHeader>
         <ModalCloseButton />
         <ModalBody>
-          <form onSubmit={handleSubmit}>
-            <FormControl>
-              <FormLabel>Repository Name</FormLabel>
-              <Input type="text" placeholder="Enter repository name" />
-            </FormControl>
-            {/* Add more form fields as needed */}
-            <Button mt="4" colorScheme="blue" type="submit">Create</Button>
-          </form>
+          <FormControl>
+            <FormLabel>Repository Name</FormLabel>
+            <Input type="text" value={repoName} onChange={(e) => setRepoName(e.target.value)} placeholder="Enter repository name" />
+          </FormControl>
         </ModalBody>
         <ModalFooter>
+          <Button colorScheme="blue" mr={3} onClick={handleSubmit}>Create</Button>
           <Button variant="ghost" onClick={onRequestClose}>Cancel</Button>
         </ModalFooter>
       </ModalContent>
